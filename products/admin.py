f71fc50bf3c3e1ac785bb8c8c7c5adc067bf8ff3
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-07 13:18:48 +0000

    Added list_display in admin.py for products

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,32 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'in_stock',
+        'image',
+    )
+
+    ordering = ('sku', )
+
+    search_fields = ('name', )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    search_fields = ('name', )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
