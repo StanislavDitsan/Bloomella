2389466b740be4ff8a0d8926a1234c3ff3976213
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-14 19:53:21 +0000

    Updated settings.py and req.txt

--- bloomella/settings.py
+++ bloomella/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-stanislavditsan-bloomell-078secg1yf.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
