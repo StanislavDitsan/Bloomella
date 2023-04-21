37675cc29f2785afe9f08aeecb1425ebbaf2cf82
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-21 22:26:30 +0000

    Updated forms.py

--- support/forms.py
+++ support/forms.py
@@ -2,11 +2,18 @@ from django import forms
 from .models import SupportTicket
 
 
+class SupportTicketForm(forms.ModelForm):
+
+    class Meta:
+        model = SupportTicket
+        fields = ['name', 'email', 'subject', 'message']
+
+
 class SupportTicketResponseForm(forms.ModelForm):
 
     class Meta:
         model = SupportTicket
-        fields = ['response']
+        fields = ['response', 'name', 'email', 'subject', 'message']
         widgets = {
             'response': forms.Textarea(attrs={'rows': 5})
         }  # optional: customize widget appearance
