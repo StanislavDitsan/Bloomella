6fe93743708cb57e28aafa192cda0d438f277fce
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-21 17:24:18 +0000

    Added model for support ticket

--- support/models.py
+++ support/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class SupportTicket(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    response = models.TextField(blank=True, null=True)
