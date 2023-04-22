c7bbf22d05eb3ff77f4a6043307751ae0580ae71
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-22 21:41:24 +0000

    Updated home page to render testimonials

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from testimonials.models import Testimonial
 
 # Create your views here.
 
 
 def index(request):
-    """ A view to return the index page """
-    return render(request, 'home/index.html')
+    """ A view to return the index page and the testimonials """
+    testimonials = Testimonial.objects.all()
+    return render(request, 'home/index.html', {'testimonials': testimonials})
