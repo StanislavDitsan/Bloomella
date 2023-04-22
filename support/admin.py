3aaf1d230f8bdcb03b7f23fee04c7d806b11a4ab
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-22 10:43:02 +0000

    Updated admin.py to send response email

--- support/admin.py
+++ support/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import SupportTicket
 from .forms import SupportTicketResponseForm
 from django.utils import timezone
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class SupportTicketAdmin(admin.ModelAdmin):
@@ -20,6 +22,14 @@ class SupportTicketAdmin(admin.ModelAdmin):
         if obj.response and change:
             # if the admin has added a response, update the updated_at field
             obj.updated_at = timezone.now()
+
+            # send email notification to user
+            subject = f"Response to your support ticket: {obj.subject}"
+            message = f"Dear {obj.name},\n\nWe have responded to your support ticket regarding '{obj.subject}':\n\n{obj.response}\n\nThank you for using our support service.\n\nBest regards,\nThe Support Team"
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = [obj.email]
+            send_mail(subject, message, from_email, recipient_list)
+
         super().save_model(request, obj, form, change)
 
 
