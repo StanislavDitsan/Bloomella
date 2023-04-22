5f3bf12b522f45de5d935085fc3321c812b63a64
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-22 16:33:34 +0000

    Created model for testimonials

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
