ef2502225c0a9450647f1879700dce22354ad868
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-22 21:40:24 +0000

    Created admin.py and approval functionality

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'created_at', 'approved')
+    list_filter = ('approved', )
+    search_fields = ('name', 'email', 'message')
+    ordering = ('-created_at', )
+
+    def approve_testimonial(self, request, queryset):
+        queryset.update(approved=True)
+
+    approve_testimonial.short_description = 'Approve selected testimonials'
+
+
+admin.site.register(Testimonial, TestimonialAdmin)
