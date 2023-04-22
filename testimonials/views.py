a74e7dc26c4b9a0840a65eabb5977ddb25066e34
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-22 21:39:20 +0000

    Created views for testimonials to display and add

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Testimonial
+from .forms import TestimonialForm
 
-# Create your views here.
+
+def testimonial_list(request):
+    testimonials = Testimonial.objects.filter(approved=True)
+    context = {'testimonials': testimonials}
+    return render(request, 'testimonials/testimonial_list.html', context)
+
+
+def add_testimonial(request):
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('testimonial_list')
+    else:
+        form = TestimonialForm()
+    context = {'form': form}
+    return render(request, 'testimonials/add_testimonial.html', context)
