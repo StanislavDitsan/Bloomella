8506c6dfb5db9d0788a5422f3e7c4d9eab0d68e7
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-23 11:52:35 +0000

    Fixed index views to filter only approved testim..

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from testimonials.models import Testimonial
 
-# Create your views here.
-
 
 def index(request):
     """ A view to return the index page and the testimonials """
-    testimonials = Testimonial.objects.all()
-    return render(request, 'home/index.html', {'testimonials': testimonials})
+    testimonials = Testimonial.objects.filter(approved=True)
+    context = {'testimonials': testimonials}
+    return render(request, 'home/index.html', context)
