6893f058f5cdd976cd2e98a2f62a0e3dc3c45f99
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-23 11:50:51 +0000

    Updated admin.py and views for testimonials

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -7,11 +7,14 @@ class TestimonialAdmin(admin.ModelAdmin):
     list_filter = ('approved', )
     search_fields = ('name', 'email', 'message')
     ordering = ('-created_at', )
+    actions = ['approve_testimonials']
 
-    def approve_testimonial(self, request, queryset):
+    def approve_testimonials(self, request, queryset):
         queryset.update(approved=True)
+        self.message_user(request,
+                          f"{len(queryset)} testimonials have been approved.")
 
-    approve_testimonial.short_description = 'Approve selected testimonials'
+    approve_testimonials.short_description = 'Approve selected testimonials'
 
 
 admin.site.register(Testimonial, TestimonialAdmin)
--- testimonials/views.py
+++ testimonials/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Testimonial
 from .forms import TestimonialForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 @login_required
 def testimonial_list(request):
-    testimonials = Testimonial.objects.filter(approved=True)
-    context = {'testimonials': testimonials}
+    approved_testimonials = Testimonial.objects.filter(approved=True)
+    context = {'testimonials': approved_testimonials}
     return render(request, 'testimonials/testimonial_list.html', context)
 
 
@@ -16,9 +17,16 @@ def add_testimonial(request):
     if request.method == 'POST':
         form = TestimonialForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('testimonial_list')
+            testimonial = form.save(commit=False)
+            testimonial.approved = False
+            testimonial.save()
+            messages.success(
+                request,
+                'Thank you for your testimonial. It will be available shortly after it is approved by an admin.'
+            )
+            return redirect('home')
     else:
         form = TestimonialForm()
+
     context = {'form': form}
     return render(request, 'testimonials/add_testimonial.html', context)
