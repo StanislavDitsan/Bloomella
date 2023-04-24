1c6c9aead4ad5fee4a6b0d26b72528d4ab4e1a50
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-04-24 15:38:36 +0000

    Added urls path for testimonial functionality

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.testimonial_list, name='testimonial_list'),
     path('add/', views.add_testimonial, name='add_testimonial'),
+    path('unapproved/',
+         views.unapproved_testimonials,
+         name='unapproved_testimonials'),
+    path('approve/<int:testimonial_id>/',
+         views.approve_testimonial,
+         name='approve_testimonial'),
 ]
