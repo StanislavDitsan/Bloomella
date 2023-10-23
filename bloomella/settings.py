e171de8f72974af3e5e9f2fdecd99d04029d2136
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-10-23 14:46:33 +0000

    Updated allowed hosts in settings.py

--- bloomella/settings.py
+++ bloomella/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['bloomella.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['bloomella.herokuapp.com', 'localhost', '.gitpod.io']
 
 # Application definition
 
