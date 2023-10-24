6b86fb8c6e269d1b41ca4f38a78d32965e3223a2
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-10-24 15:16:38 +0000

    Displaying some products on index page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from testimonials.models import Testimonial
+from products.models import Product, Category
 
 
 def index(request):
     """ A view to return the index page and the testimonials """
     testimonials = Testimonial.objects.filter(approved=True)
-    context = {'testimonials': testimonials}
+
+    products = Product.objects.all()[:4]
+    all_categories = Category.objects.exclude(name='all_products')
+
+    context = {
+        'testimonials': testimonials,
+        'products': products,
+        'all_categories': all_categories,
+    }
     return render(request, 'home/index.html', context)
